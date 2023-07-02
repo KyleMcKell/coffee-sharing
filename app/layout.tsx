9303051da19fc1364fb1f5f49e353f8ff09a0a33
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2023-07-02 16:01:17 -0600

    added clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,29 @@
-import './globals.css';
-import { Inter } from 'next/font/google';
+import "./globals.css"
+import { Inter } from "next/font/google"
+import { ClerkProvider } from "@clerk/nextjs"
 
 export const metadata = {
-  title: 'Vercel Postgres Demo with Drizzle',
+  title: "Vercel Postgres Demo with Drizzle",
   description:
-    'A simple Next.js app with Vercel Postgres as the database and Drizzle as the ORM',
-};
+    "A simple Next.js app with Vercel Postgres as the database and Drizzle as the ORM",
+}
 
 const inter = Inter({
-  variable: '--font-inter',
-  subsets: ['latin'],
-  display: 'swap',
-});
+  variable: "--font-inter",
+  subsets: ["latin"],
+  display: "swap",
+})
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.variable}>{children}</body>
-    </html>
-  );
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.variable}>{children}</body>
+      </html>
+    </ClerkProvider>
+  )
 }

c094f63f33e6c9e08f8363643b94771d72ed7ba3
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2023-07-14 00:11:47 -0600

    bro what is going on lol

--- src/db/migrations/0000_dizzy_sersi.sql
+++ src/db/migrations/0000_dizzy_sersi.sql
@@ -64,6 +64,22 @@ CREATE TABLE IF NOT EXISTS "verification_token" (
 --> statement-breakpoint
 ALTER TABLE "verification_token" ADD CONSTRAINT "verification_token_identifier_token" PRIMARY KEY("identifier","token");
 --> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "barista_idx" ON "coffee_bean" ("barista_id");--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "coffee_bean_idx" ON "coffee_bean_tasting_note" ("coffee_bean_id");--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "tasting_note_idx" ON "coffee_bean_tasting_note" ("tasting_note_id");--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "user_idx" ON "session" ("user_id");--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "account" ADD CONSTRAINT "account_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "coffee_bean" ADD CONSTRAINT "coffee_bean_barista_id_user_id_fk" FOREIGN KEY ("barista_id") REFERENCES "user"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
 DO $$ BEGIN
  ALTER TABLE "coffee_bean_tasting_note" ADD CONSTRAINT "coffee_bean_tasting_note_coffee_bean_id_coffee_bean_id_fk" FOREIGN KEY ("coffee_bean_id") REFERENCES "coffee_bean"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
@@ -72,6 +88,12 @@ END $$;
 --> statement-breakpoint
 DO $$ BEGIN
  ALTER TABLE "coffee_bean_tasting_note" ADD CONSTRAINT "coffee_bean_tasting_note_tasting_note_id_tasting_note_id_fk" FOREIGN KEY ("tasting_note_id") REFERENCES "tasting_note"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "tasting_note" ADD CONSTRAINT "tasting_note_barista_id_user_id_fk" FOREIGN KEY ("barista_id") REFERENCES "user"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
